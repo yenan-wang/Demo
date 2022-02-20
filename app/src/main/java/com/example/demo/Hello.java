4df731e0bfa4861294bb341a020617c439c1da29
Wang <3428141843@qq.com>
2022-02-20 16:31:43 +0800

    [Demo][1/1]{1、去掉.idea目录下的文件提交} 适用范围：{All} 准入id：{无} 分析：{本地构建目录，无需提交到远程仓库} 方案：{无} 风险及影响：{无} 测试建议：{} 其它：{}

---
+++ app/src/main/java/com/example/demo/Hello.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class Hello {
+    public static void main(String [] args) {
+
+        System.out.println("invoke success!");
+        System.out.println("Hello World!");
+
+    }
+}
